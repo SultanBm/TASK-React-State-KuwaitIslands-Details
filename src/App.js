c80a206ee77f53c98145df257950948b9fceaa2a
sultan beddah <sultanbeddah@gmail.com>
2022-05-11 15:06:45 +0300

    gg

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-
-import './App.css';
-import Header from './Components/Header';
-import IslandForm from './Components/IslandForm';
-import IslandList from './Components/IslandList';
-
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import IslandForm from "./Components/IslandForm";
+import IslandList from "./Components/IslandList";
 
 function App() {
-
-  let island = {
+  const [island, setIsland] = useState({
     name: "Boubyan",
-    img: "http://photos.wikimapia.org/p/00/02/20/48/92_big.jpg"
-  }
+    img: "http://photos.wikimapia.org/p/00/02/20/48/92_big.jpg",
+  });
 
   return (
-
     <div>
       <Header />
-
       <div className="homePage">
-        <IslandList />
+        <IslandList setIsland={setIsland} />
         <IslandForm island={island} />
       </div>
     </div>
--- src/Components/IslandForm.js
+++ src/Components/IslandForm.js
@@ -5,7 +5,16 @@ export default function IslandForm({ island }) {
       <img src={island.img} alt={island.name} />
       <h3>Book a trip to {island.name} island</h3>
       <input placeholder="Type Full Name" />
-      <input placeholder="Type Phone Number" />
+      <input
+        type="tel"
+        id="telNo"
+        name="telNo"
+        placeholder="Type Phone Number"
+        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+        maxLength={8}
+        minLength={8}
+        required
+      />
       <button className="book" onClick={() => {}}>
         Book for today!
       </button>
